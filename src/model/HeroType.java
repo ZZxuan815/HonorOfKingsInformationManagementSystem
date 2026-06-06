42dea303e4794a56cef5706f879059be7968c20b
HUAWEI <HUAWEI@LAPTOP-0K81ED52>
2026-06-06 14:58:14 +0800

    [AI-Architect] Create HeroType, MatchResult enums and revise Plan.md

---
+++ src/model/HeroType.java
@@ -0,0 +1,9 @@
+package model;//英雄的职业（算是定位吧）
+public enum HeroType {
+	    WARRIOR,    // 战士
+	    MAGE,       // 法师
+	    ASSASSIN,   // 刺客
+	    TANK,       // 坦克
+	    SUPPORT,    // 辅助
+	    MARKSMAN,   // 射手
+}
